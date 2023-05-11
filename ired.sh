197404e666c79bb98865ec4b4d7a4807c589d2de
Gambled23 <ipog71@gmail.com>
2023-05-11 12:38:36 -0600

    dns

--- ired.sh
+++ ired.sh
@@ -5,8 +5,11 @@ iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 #Aceptar localhost
 iptables -A INPUT -i lo -j ACCEPT
 
-# Loop device.
-iptables -A INPUT -i lo -j ACCEPT
+# DNS
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
 
 # http/https, smtp/smtps, pop3/pop3s, imap/imaps, ssh
 iptables -A INPUT -p tcp -m multiport --dport 80,443,25,465,110,995,143,993,587,465,22 -j ACCEPT
@@ -32,17 +35,11 @@ iptables -A INPUT -p tcp -m multiport --dport 143,993 -j ACCEPT
 # ldap/ldaps
 iptables -A INPUT -p tcp -m multiport --dport 389,636 -j ACCEPT
 
-# ftp.
-iptables -A INPUT -p tcp -m multiport --dport 21,20 -j ACCEPT
+# ftp
+iptables -A INPUT -p tcp -m multiport --dport 21,20,990,989 -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dport 21,20,990,989 -j ACCEPT
 
-# ssh
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# DNS
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p udp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
-iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
 # mysql
 iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 3306 -j ACCEPT
