065116c55c706971686bdb3b1262deb4daa1d716
Gambled23 <ipog71@gmail.com>
2023-05-11 09:44:08 -0600

    lets fucking go

--- meme.sh
+++ meme.sh
@@ -15,10 +15,14 @@ iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 #ssh
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
+#DNS
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
 #Restringir input
 
-iptables -A INPUT -j DROP
+iptables -P INPUT DROP
 #Restringir output
 
-iptables -A OUTPUT -j DROP
+iptables -P OUTPUT DROP
