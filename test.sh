2603c1c317346748decdc3eab6dc77e33ee85779
Gambled23 <ipog71@gmail.com>
2023-05-20 09:21:27 -0600

    soto 11

--- test.sh
+++ test.sh
@@ -9,54 +9,57 @@ iptables -X
 iptables -Z
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
+#FIREWALL
+iptables -N FIREWALL
+iptables -A INPUT -j FIREWALL
+iptables -A OUTPUT -j FIREWALL
+
 # ssh
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 22 -j ACCEPT
 
 # web/s
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 443 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8443 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 8080 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 8443 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 80 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 443 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 8080 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 8443 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 8080 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 8443 -j ACCEPT
 
 #samba
-iptables -A INPUT -p udp -m udp --dport 137 -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 138 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 139 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 445 -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --sport 137 -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --sport 138 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 139 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 445 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --dport 137 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --dport 138 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 139 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 445 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --sport 137 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --sport 138 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 139 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 445 -j ACCEPT
 #cups
-iptables -A INPUT -p tcp -m tcp --dport 631 -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 631 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 631 -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --sport 631 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 631 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --dport 631 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 631 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --sport 631 -j ACCEPT
 
 # dns
-iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 53 -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --sport 53 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 53 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --dport 53 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --sport 53 -j ACCEPT
+iptables -A FIREWALL -p udp -m udp --sport 53 -j ACCEPT
 
 # subversion
-iptables -A INPUT -p tcp -m tcp --dport 3690 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 3690 -j ACCEPT
 
-# ftp + active ftp + pasv ftp
-iptables -A INPUT -p tcp --dport 21 -m state --state ESTABLISHED,NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 20 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 990 -m state --state ESTABLISHED,NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 989 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 50000:60000 -m state --state RELATED,ESTABLISHED,NEW -j ACCEPT
+# ftp + ftp activo + ftp pasivo
+iptables -A FIREWALL -p tcp --dport 21 -m state --state ESTABLISHED,NEW -j ACCEPT
+iptables -A FIREWALL -p tcp --dport 20 -m state --state ESTABLISHED -j ACCEPT
+iptables -A FIREWALL -p tcp --dport 990 -m state --state ESTABLISHED,NEW -j ACCEPT
+iptables -A FIREWALL -p tcp --dport 989 -m state --state ESTABLISHED -j ACCEPT
+iptables -A FIREWALL -p tcp --dport 50000:60000 -m state --state RELATED,ESTABLISHED,NEW -j ACCEPT
 
 # mysql
-iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+iptables -A FIREWALL -p tcp -m tcp --dport 3306 -j ACCEPT
 
 # ping cliente-servidor
 # iptables -A INPUT -p icmp -j ACCEPT
